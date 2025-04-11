a749e2a6e1ae0eddd7c87829ccb8ec0be09e32bd
Allen Tan <tanallenbernard@gmail.com>
2025-04-11 09:04:53 +0800

    add Toaster component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
-<script setup></script>
+<script setup>
+import { Toaster, ToastProvider } from '@/components/ui/toast'
+</script>
 
 <template>
   <div class="min-h-screen bg-background text-foreground">
-    <router-view />
+    <ToastProvider>
+      <router-view />
+      <Toaster />
+    </ToastProvider>
   </div>
 </template>
