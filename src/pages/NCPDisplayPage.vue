b8adc9e30d44ff03965c8373fc24d20d7f648af6
Allen Tan <tanallenbernard@gmail.com>
2025-04-12 18:08:52 +0800

    add NCPDisplayPage component with routing and layout integration

---
+++ src/pages/NCPDisplayPage.vue
@@ -0,0 +1,14 @@
+<script setup>
+import NCPDisplay from '@/components/ncp/NCPDisplay.vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const ncp = route.params.ncp
+const format = route.params.format
+</script>
+
+<template>
+  <DefaultLayout>
+    <NCPDisplay :ncp="ncp" :format="format" />
+  </DefaultLayout>
+</template>
